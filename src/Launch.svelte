ab5755b358b834e488bb73f2b67c40c833145954
Thor Stenbæk <tst@dips.no>
2021-07-08 13:57:31 +0200

    Launch against Selecta (Bodø students) FHIR API

--- src/Launch.svelte
+++ src/Launch.svelte
@@ -4,14 +4,16 @@
 
     onMount(async () => {
 		Smart.authorize({
-			clientId: 'dips-ehr-images',
+			clientId: 'dips-ehr-pdf',			
 			scope: 'launch launch/patient patient/DocumentReference.write patient/DocumentReference.read patient/Patient.read patient/encounter patient/resource online_access openid fhirUser',			
 			redirectUri: './app',			
 
-			iss:		  'https://launch.smarthealthit.org/v/r3/sim/'
+			/*iss:		  'https://launch.smarthealthit.org/v/r3/sim/'
 			  			+ 'eyJoIjoiMSIsImIiOiJmMDQ2MjkzNi1lYjRiLTRkYT'
 			  			+ 'EtYjQ1YS1mYmQ5NmViZjhjY2IiLCJlIjoic21hcnQt'
-			  			+ 'UHJhY3RpdGlvbmVyLTcxNjE0NTAyIn0/fhir',
+			  			+ 'UHJhY3RpdGlvbmVyLTcxNjE0NTAyIn0/fhir',*/
+			iss: 'https://vt-selecta-b.dips.local/DIPS-WebAPI/HL7/FHIR-R4',
+			launch: '6c5d9ca9-54d7-42f5-bfae-a7c19cd217f2::DocumentReference/c1c5fdad-eda4-4e9d-89b0-2a7d8eed45cb'
 
 		});
 	});
